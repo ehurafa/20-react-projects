47d299efd00fbb9dc2caf59e7a7c8f5be7d9bc67
Rafael Gomes <rafarfl2010@gmail.com>
2023-11-23 16:46:56 -0300

    # SECTION 1 - (Calculator): Initial setup

---
+++ 01-calculator/calculator/src/components/Calculator.jsx
@@ -0,0 +1,13 @@
+
+const Calculator = () => {
+  return (
+   <div className="calculator">
+    <div className="complete-operation">xxx</div>
+    <div className="display">000</div>
+    <div className="buttons"></div>
+    <button>=</button>
+   </div>
+  )
+}
+
+export default Calculator
