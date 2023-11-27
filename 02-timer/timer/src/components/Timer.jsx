546d6eca55dbc0bd5047592c03def94e09bc10d7
ehurafa <rafarfl2010@gmail.com>
2023-11-27 19:35:01 -0400

    (Timer):  starting and stopping the timer

--- 02-timer/timer/src/components/Timer.jsx
+++ 02-timer/timer/src/components/Timer.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+
 import TimerDisplay from './TimerDisplay';
 import TimerControls from './TimerControls';
 import LapList from './LapList';
@@ -5,10 +7,49 @@ import LapList from './LapList';
 import "./Timer.css";
 
 const Timer = () => {
+
+    const [millisenconds, setMilliseconds] = useState(0);
+    const [timeOn, setTimeOn] = useState(false);
+    const [laps, setLaps] = useState([]);
+
+    const formatTime = () => {
+        const minutes = ("0" + Math.floor(millisenconds / 60000) % 60).slice(-2);
+        const seconds = ("0" + Math.floor(millisenconds / 1000) % 60).slice(-2);
+        const centiseconds = ("0" + Math.floor(millisenconds / 10) % 100).slice(-2);
+
+        return `${ minutes }:${ seconds }:${ centiseconds }`
+    } 
+
+    const startTimer = (interval) => {
+        return setInterval(() => {
+            setMilliseconds(prevMilliseconds => prevMilliseconds + 10)
+        }, 10);
+    };
+
+    const stopTimer = (interval) => {
+        clearInterval(interval);
+        return interval;
+    }
+
+    useEffect(() => {
+        let interval = null;
+
+        if (timeOn) {
+            interval = startTimer(interval);
+        } else {
+            interval = stopTimer(interval);
+        }
+
+        return () => stopTimer(interval)
+    }, [timeOn]);
+
   return (
     <div className="timer-container">
-        <TimerDisplay />
-        <TimerControls />
+        <TimerDisplay time={ formatTime() } />
+        <TimerControls 
+            onStart={ ()=> setTimeOn(true) }
+            onStop={ ()=> setTimeOn(false) }
+        />
         <LapList />
     </div>
 
