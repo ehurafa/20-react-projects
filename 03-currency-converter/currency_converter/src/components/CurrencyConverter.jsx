87b0c12509fb90f0ad12721adeeb0fd71c34417f
ehurafa <rafarfl2010@gmail.com>
2023-11-29 20:10:09 -0400

    (currency):  get API data

--- 03-currency-converter/currency_converter/src/components/CurrencyConverter.jsx
+++ 03-currency-converter/currency_converter/src/components/CurrencyConverter.jsx
@@ -1,20 +1,46 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 import "./CurrencyConverter.css";
 
 const CurrencyConverter = () => {
+
+    const [rates, setRates] = useState(null);
+    const [fromCurrency, setFromCurrency] = useState("USD");
+    const [toCurrency, setToCurrency] = useState("EUR");
+    const [amount, setAmount] = useState(1);
+    const [convertedAmount, setConvertedAmount] = useState(null);
+
+    useEffect(() => {
+
+        axios.get("https://v6.exchangerate-api.com/v6/1af6660fb2954ecd1cbcde80/latest/USD")
+            .then((response) => {
+                setRates(response.data.conversion_rates);
+            })
+            .catch((error) => {
+                console.log("Ocorreu um erro: ", error);
+            })
+
+    }, []);
+
   return (
     <div className="converter">
         <h2>Conversor de Moedas</h2>
-        <input type="number" placeholder="Digite o valor..." />
+        <input type="number" placeholder="Digite o valor..." value={ amount } />
         <span>Selecione as moedas</span>
-        <select>
-            <option value="BRL">BRL</option>
+        <select value={ fromCurrency }>
+            { Object.keys(rates).map((currency) => (
+                <option value={currency} key={currency}>{currency}</option>
+            ))}
         </select>
         <span>para</span>
-        <select>
-            <option value="USD">USD</option>
+        <select value={ toCurrency }>
+            { Object.keys(rates).map((currency) => (
+                <option value={currency} key={currency}>{currency}</option>
+            ))}
         </select>
-        <h3>BRL USD</h3>
-        <p>20 BRL vale 5 USD</p>
+        <h3>{ convertedAmount } { toCurrency }</h3>
+        <p>{ amount } { fromCurrency } vale { convertedAmount } { toCurrency }</p>
     </div>
   )
 }
