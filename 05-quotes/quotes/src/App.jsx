b74295e134e6fbdad7a5ecd41ea918207630665f
ehurafa <rafarfl2010@gmail.com>
2023-12-05 20:23:24 -0400

    (quotes):  Displaying quotes on button click

--- 05-quotes/quotes/src/App.jsx
+++ 05-quotes/quotes/src/App.jsx
@@ -1,8 +1,21 @@
-import './App.css'
+import Quote from "./components/Quote";
+
+import quotes from "./data";
+
+import { useState } from "react";
 
 function App() {
+  const [index, setIndex] = useState(0)
+
+  const nextQuote = () => {
+    setIndex((current) => (current + 1) % quotes.length);
+  }
+
   return (
-    <>quotes</>
+    <div className="container mt-5"> 
+      <p>{ quotes[index].text}</p>
+      <button className="btn btn-success mt-2" onClick={nextQuote}>Próxima citação</button>
+    </div>
   )
 }
 
--- 05-quotes/quotes/src/data.js
+++ 05-quotes/quotes/src/data.js
@@ -1,62 +1,62 @@
 // src/data.js
 
-const citacoes = [
+const quotes = [
     {
-      autor: "Machado de Assis",
-      texto:
+      author: "Machado de Assis",
+      text:
         "A vida não é mais que um sonho; mas nesse sonho não há nem riso nem prazer.",
     },
     {
-      autor: "Clarice Lispector",
-      texto: "Liberdade é pouco. O que eu desejo ainda não tem nome.",
+      author: "Clarice Lispector",
+      text: "Liberdade é pouco. O que eu desejo ainda não tem nome.",
     },
     {
-      autor: "Carlos Drummond de Andrade",
-      texto: "Ser feliz sem motivo é a mais autêntica forma de felicidade.",
+      author: "Carlos Drummond de Andrade",
+      text: "Ser feliz sem motivo é a mais autêntica forma de felicidade.",
     },
     {
-      autor: "Mario Quintana",
-      texto: "Eles passarão... eu passarinho!",
+      author: "Mario Quintana",
+      text: "Eles passarão... eu passarinho!",
     },
     {
-      autor: "Paulo Freire",
-      texto:
+      author: "Paulo Freire",
+      text:
         "Se a educação sozinha não transforma a sociedade, sem ela tampouco a sociedade muda.",
     },
     {
-      autor: "Fernando Pessoa",
-      texto:
+      author: "Fernando Pessoa",
+      text:
         "O valor das coisas não está no tempo que elas duram, mas na intensidade com que acontecem. Por isso, existem momentos inesquecíveis, coisas inexplicáveis e pessoas incomparáveis.",
     },
     {
-      autor: "Vinicius de Moraes",
-      texto:
+      author: "Vinicius de Moraes",
+      text:
         "Que não seja imortal, posto que é chama, mas que seja infinito enquanto dure.",
     },
     {
-      autor: "Monteiro Lobato",
-      texto: "Um país se faz com homens e livros.",
+      author: "Monteiro Lobato",
+      text: "Um país se faz com homens e livros.",
     },
     {
-      autor: "Guimarães Rosa",
-      texto:
+      author: "Guimarães Rosa",
+      text:
         "O real não está na saída nem na chegada: ele se dispõe para a gente é no meio da travessia.",
     },
     {
-      autor: "Lya Luft",
-      texto:
+      author: "Lya Luft",
+      text:
         "Perder-se significa ir achando e nem saber o que fazer do que for achando.",
     },
     {
-      autor: "Adélia Prado",
-      texto:
+      author: "Adélia Prado",
+      text:
         "Quero cotidiano. Mudei de quereres tantas vezes, estou só esperando virar amor.",
     },
     {
-      autor: "Cora Coralina",
-      texto:
+      author: "Cora Coralina",
+      text:
         "Recria tua vida, sempre, sempre. Remove pedras e planta roseiras e faz doces. Recomeça.",
     },
   ];
   
-  export default citacoes;
+  export default quotes;
