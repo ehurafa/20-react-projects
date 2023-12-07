ba594ee1930c40cee4e628c7141e0749ab9ee6f4
ehurafa <rafarfl2010@gmail.com>
2023-12-06 22:21:44 -0400

    (photos):  Initial styles

--- 06-photo-album/photo-album/src/components/Searchbar.jsx
+++ 06-photo-album/photo-album/src/components/Searchbar.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
 const Searchbar = () => {
+
+    const categories = ["Natureza", "Pessoas", "Tecnologia", "Animais", "Esportes"];
+
   return (
-    <div>Searchbar</div>
+    <div className="search-bar">
+        <input type="text" placeholder="Pesquisar fotos..." />
+        <button>Pesquisar</button>
+        <select>
+            { categories.map((category) => (
+                <option key={ category } value={ category }>
+                    { category }
+                </option>
+            ))}
+        </select>
+    </div>
   )
 }
 
