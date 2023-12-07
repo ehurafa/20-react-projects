83e500c4d1859b19fe182ae0b7ca48959d3ef28a
ehurafa <rafarfl2010@gmail.com>
2023-12-06 20:51:52 -0400

    (photos): Api Configuration

--- 06-photo-album/photo-album/src/App.jsx
+++ 06-photo-album/photo-album/src/App.jsx
@@ -1,10 +1,31 @@
 import { useState, useEffect } from "react";
 
-import DetailedPhoto from './components/DetailedPhoto'
-import PhotoList from './components/PhotoList'
-import Searchbar from './components/Searchbar'
+import DetailedPhoto from './components/DetailedPhoto';
+import PhotoList from './components/PhotoList';
+import Searchbar from './components/Searchbar';
+
+import axios from "axios";
 
 function App() {
+
+  const [query, setQuery] = useState("");
+  const [category, setCategory] = useState("");
+
+  const fetchData = async({ query, category }) => {
+    const apiKey = import.meta.env.VITE_UNSPLASH_API_KEY;
+
+    const response = await axios.get(`https://api.unsplash.com/photos/random`, {
+      params: {
+        client_id: apiKey
+      }
+    });
+
+    console.log(response)
+  };
+
+  useEffect(() => {
+    fetchData(query, category);
+  }, []);
 
   return (
     <div className="container">
