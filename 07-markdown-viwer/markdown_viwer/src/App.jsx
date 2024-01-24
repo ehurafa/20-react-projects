1c59068c449be1b39164928e83fecf7ab2c5bbe0
Rafael Gomes <rafarfl2010@gmail.com>
2024-01-23 21:52:53 -0300

    feat(markdown viwer) Converting markdown to html

--- 07-markdown-viwer/markdown_viwer/src/App.jsx
+++ 07-markdown-viwer/markdown_viwer/src/App.jsx
@@ -1,14 +1,28 @@
 import { useState, useEffect, useRef } from 'react'
 
+import { marked } from "marked"
+ 
 import Toolbar from "./components/Toolbar"
 
 function App() {
 
+  const [text, setText] = useState("# Olá, eu sou feito de markdown");
+
+  const renderText = () => {
+    return { __html: marked(text) };
+  }
+
+  const textAreaRef = useRef(null);
+
   return (
     <div className="app-container">
       <Toolbar />
-      <textarea></textarea>
-      <div></div>
+      <textarea 
+        ref={textAreaRef} 
+        value={text} 
+        onChange={ (e) => setText(e.target.value) }>
+      </textarea>
+      <div dangerouslySetInnerHTML={ renderText() }/>
     </div>
   )
 }
--- 07-markdown-viwer/markdown_viwer/src/components/Toolbar.tsx
+++ 07-markdown-viwer/markdown_viwer/src/components/Toolbar.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Toolbar = () => {
   return (
-    <div>Toolbar</div>
+    <div className="toolbar">
+      <button>H1</button>
+      <button>H2</button>
+      <button>Bold</button>
+      <button>Itálico</button>
+      <button>Bloco de código</button>
+      <button>Lista de itens</button>
+      <button>Linha horizontal</button>
+    </div>
   )
 }
 
