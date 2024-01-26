6658c1e756ce084474876a50b1f293eede651559
Rafael Gomes <rafarfl2010@gmail.com>
2024-01-26 17:54:05 -0300

    feat(markdown viwer) Initial styles.

--- 07-markdown-viwer/markdown_viwer/src/App.jsx
+++ 07-markdown-viwer/markdown_viwer/src/App.jsx
@@ -6,13 +6,18 @@ import Toolbar from "./components/Toolbar"
 
 function App() {
 
-  const [text, setText] = useState("# Olá, eu sou feito de markdown");
+  const [text, setText] = useState(
+    localStorage.getItem('markdownText') ||"# Olá, eu sou feito de markdown");
 
   const renderText = () => {
     return { __html: marked(text) };
   }
 
   const textAreaRef = useRef(null);
+
+  useEffect(() => {
+    localStorage.setItem("markdownText", text);
+  }, [text]);
 
   return (
     <div className="app-container">
