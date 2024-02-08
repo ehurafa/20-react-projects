aae57085efd1b7d6e5102feb3181c19d72035e6e
Rafael Gomes <rafarfl2010@gmail.com>
2024-02-07 22:39:21 -0300

    feat(timezone): initial config

---
+++ 08-time-zone/time-zone/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Relógio Mundial</h1>
+      <select>
+        <option value="" disabled select>Selecione o fusu horário</option>
+      </select>
+      <div></div>
+    </div>
+  )
+}
+
+export default App
---
+++ 08-time-zone/time-zone/src/components/TimeZoneClock.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react'
+
+const TimeZoneClock = () => {
+  return (
+    <div>
+        <h2>timezone</h2>
+        <h3>time</h3>
+    </div>
+  )
+}
+
+export default TimeZoneClock
