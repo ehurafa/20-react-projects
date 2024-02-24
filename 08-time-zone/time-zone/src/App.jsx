c291f0ee42de37c582820e90e2fb20a2733cee83
Rafael Gomes <rafarfl2010@gmail.com>
2024-02-23 21:30:26 -0300

    feat(timezone) stles and timezone options

--- 08-time-zone/time-zone/src/App.jsx
+++ 08-time-zone/time-zone/src/App.jsx
@@ -18,16 +18,32 @@ function App() {
   ];
 
   const localTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  const [selectedTimeZones, setSelectedTimeZones] = useState([localTimeZone]);
-  console.log('localTimeZone ', selectedTimeZones)
+  const [selectedTimeZones, setSelectedTimeZones] = useState([localTimeZone]);  
+
+  const addTimeZone = (e) => {
+    const newTime = e.target.value;
+
+    console.log(selectedTimeZones)
+
+    if(!selectedTimeZones.includes(newTime)) {
+      setSelectedTimeZones(...selectedTimeZones, newTime);
+      console.log(selectedTimeZones)
+    }
+  }
 
   return (
     <div>
       <h1>Relógio Mundial</h1>
-      <select>
+      <select onChange={(e) => addTimeZone(e)}>
         <option value="" disabled select>Selecione o fusu horário</option>
+        {timezones.map((time) => (
+          <option value={time} key={time}>{time}</option>
+        ))}
       </select>
       <div>
+        <hr />
+        <p>{ selectedTimeZones }</p>
+        <hr />
         { selectedTimeZones && selectedTimeZones.map((timezone) => (
           <TimeZoneClock key={timezone} timeZone={timezone} />
         ))}
