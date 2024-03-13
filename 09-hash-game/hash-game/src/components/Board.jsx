7fb29ed6d9d08756d429db19532d527fe092ee71
Rafael Gomes <rafarfl2010@gmail.com>
2024-03-13 17:35:41 -0300

    feat(hashgame): Initial setup

---
+++ 09-hash-game/hash-game/src/components/Board.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react'
+import Square from './Square'
+
+const Board = () => {
+  return (
+    <div>
+        <Square />
+    </div>
+  )
+}
+
+export default Board
