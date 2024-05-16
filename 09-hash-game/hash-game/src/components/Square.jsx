cf0933c553dc129680702c88fbf165836da7e675
Rafael Gomes <rafarfl2010@gmail.com>
2024-05-16 17:18:49 -0300

    start the logic

--- 09-hash-game/hash-game/src/components/Square.jsx
+++ 09-hash-game/hash-game/src/components/Square.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Square = () => {
+const Square = ({ value, onClick }) => {
   return (
-    <button className="square"></button>
+    <button className="square" onClick={onClick}>{ value }</button>
   )
 }
 
