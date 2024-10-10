68cbfc944349b10f1b1dd3356043962d25a87410
Rafael Gomes <rafarfl2010@gmail.com>
2024-10-10 20:31:13 -0300

    feat(hashgame): winner condition

--- 09-hash-game/hash-game/src/components/Board.jsx
+++ 09-hash-game/hash-game/src/components/Board.jsx
@@ -4,7 +4,7 @@ import Square from './Square'
 const Board = () => {
   const [isNext, setIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null))
-  const winner = false;
+  const winner = calculateWinner(squares)
 
   const [aiIsThinking, setAiIsThinking] = useState(false);
 
@@ -55,6 +55,27 @@ const Board = () => {
       <button className="reset-button" onClick={resetGame}>Reiniciar jogo</button>
     </div>
   )
+}
+
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
+  for(let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+      return squares[a]
+    }
+    return null
+  }
 }
 
 export default Board
