37f6b85ba28a4d94ce2405a5c21ee65dafd778b4
Rafael Gomes <rafarfl2010@gmail.com>
2024-10-15 21:44:03 -0300

    feat(todo_redux): Initial setup

---
+++ 10-todo-redux/todo_redux/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import AddTodo from './components/AddTodo'
+import TodoList from './components/TodoList'
+
+function App() {
+
+  return (
+    <div className="container">
+      <h1>Lista de tarefas com redux</h1>
+      <AddTodo />
+      <TodoList />
+    </div>
+  )
+}
+
+export default App
